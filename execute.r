5b6b69fb90fe9bf90378701d61a17530af1a42d5
Fonseca367 <fonsecaalessandra024@gmail.com>
2025-07-04 11:32:54 -0300

    Update execute.r

--- execute.r
+++ execute.r
@@ -1,5 +1,5 @@
 library(reticulate)
-py_install("git+https://github.com/souzathw/semlite.git", pip = TRUE)
+py_install("git+https://github.com/Fonseca367/semlita.git", pip = TRUE)
 install.packages("lavaan")
 sem <- import("semlite.moderation")
 
